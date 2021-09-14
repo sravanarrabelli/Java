1c8988d62891cd661ff2b50dcee71ff8a8550560
sravanarrabelli <90613438+sravanarrabelli@users.noreply.github.com>
2021-09-14 11:48:22 +0530

    Add files via upload

---
+++ Polindromnum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Polindromnum
+{
+ public static void main(String[] args)
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter a number");
+  int num=sc.nextInt();
+  int org_num=num;
+  int rev=0;
+  while(num!=0)
+  {
+   rev=rev*10+num%10;
+   num=num/10;
+  }
+   if(org_num==rev)
+    {
+     System.out.println(org_num+"Polindrom number");
+    }
+    else{
+      System.out.println(org_num+"Is not a Polindrom number");
+      }
+ }
+}
